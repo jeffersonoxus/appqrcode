0f2ab8f98d27a0753b6307cf7674620ab9cbab81
Jefferson David <jeffersonoxus@gmail.com>
2026-08-01 18:03:38 -0300

    link leitor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>App QR Code</h1>
+    <main className="p-4 max-w-md mx-auto">
+      <h1 className="text-xl font-bold mb-4 text-center">Gabarito App</h1>
+
+      <Link
+        href="/leitor"
+        className="block w-full text-center py-3 bg-blue-600 text-white rounded-lg font-medium"
+      >
+        Abrir Leitor de QR Code
+      </Link>
     </main>
   );
 }
